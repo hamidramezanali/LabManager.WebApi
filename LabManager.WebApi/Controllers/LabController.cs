3ad33ac98ebb65eede75aed1b57b7f93acad498e
hamidramezanali <hamid.ra2018@gmail.com>
2022-09-04 17:45:05 +0200

    Added blocking collection to the controller

--- LabManager.WebApi/Controllers/LabController.cs
+++ LabManager.WebApi/Controllers/LabController.cs
@@ -2,6 +2,7 @@
 using LabManager.WebApi.Model;
 using LabManager.WebApi.Service;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 
 namespace LabManager.WebApi.Controllers
@@ -37,38 +38,80 @@ namespace LabManager.WebApi.Controllers
         [HttpGet("{name}")]
         public async Task<IActionResult> Run(string name)
         {
-            string outputText = string.Empty;
-            var standardError = string.Empty;
+  
 
             try
             {
-                using (Process process = new Process())
-                {
-                    process.StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "dotnet",
-                        Arguments = @$"run --project WaitingTest.csproj",
-                        WorkingDirectory = @"C:\Users\hamid\source\repos\WaitingTest\WaitingTest\",//the file must exist
-                        RedirectStandardOutput = true,
-                        RedirectStandardError = true,
-                        UseShellExecute = false,
-                        CreateNoWindow = false,
-                    };
-
-                    process.Start();
-                    outputText = process.StandardOutput.ReadToEnd();
-                    outputText = outputText.Replace(Environment.NewLine, string.Empty);
-                    standardError = process.StandardError.ReadToEnd();
-                    process.WaitForExit();
-                }
+               await  Task.Factory.StartNew(async () => await ProcessAsync(new BlockingCollection<QueueItem>
+                   ()
+               { new QueueItem()
+               {
+                   OrderId=Guid.NewGuid(),
+                   Request=new SampleInQueueRequest(){Name="HAMID"}
+               }
+               }), TaskCreationOptions.LongRunning);
             }
             catch (Exception ex)
             {
 
             }
             var stringjsonData = @"{'FirstName': 'Jignesh', 'LastName': 'Trivedi'}";
-            return Ok(stringjsonData + standardError);
+            return Ok(stringjsonData );
         }
 
+        private async Task ProcessEachQueueItemAsync(QueueItem sample)
+        {
+            string outputText = string.Empty;
+            var standardError = string.Empty;
+            using (Process process = new Process())
+            {
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "dotnet",
+                    Arguments = @$"run --project WaitingTest.csproj",
+                    WorkingDirectory = @"C:\Users\hamid\source\repos\WaitingTest\WaitingTest\",//the file must exist
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = false,
+                };
+
+                process.Start();
+                outputText = process.StandardOutput.ReadToEnd();
+                outputText = outputText.Replace(Environment.NewLine, string.Empty);
+                standardError = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+            }
+        }
+        private async Task ProcessAsync(BlockingCollection<QueueItem> queue)
+        {
+            foreach (var item in queue)
+            {
+                await ProcessEachQueueItemAsync(item);
+            }
+        }
+        public class CheckoutResponse
+        {
+            public Guid OrderId { get; set; }
+
+            public OrderStatus OrderStatus { get; set; }
+            public string Message { get; set; }
+        }
+        public class QueueItem
+        {
+            public Guid OrderId { get; set; }
+            public SampleInQueueRequest Request { get; set; }
+        }
+    }
+
+    public enum OrderStatus
+    {
+        Failed,
+        OK
+    }
+
+    public class SampleInQueueRequest
+    {
+        public string Name { get; set; }
     }
 }
